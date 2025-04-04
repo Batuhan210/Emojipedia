8e774ed8d655a4765ca98dc2dc12a3616e84ecaa
Batuhan <92650261+Batuhan210@users.noreply.github.com>
2025-04-04 15:31:37 +0300

    Update emojipedia.js

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -26,6 +26,62 @@ const emojipedia = [
         emoji: "🤓",
         name: "Nerd Face",
         meaning:"Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+      },
+
+      {
+         id: 5,
+         emoji: "😎",
+         name: "Face With Sunglasses",
+         meaning: "Coolness, self confidence, relaxation, pride, summer, irony",
+      },
+
+      {
+        id: 6,
+        emoji: "😏",
+        name: "Smirking Face",
+        meaning: "A smirk is a smile that expresses smugness, scorn, or insincerity. The smirking face emoji is often used to convey a sense of smugness or self-satisfaction."
+      },
+
+      {
+        id: 7,
+        emoji: "😱",
+        name: "Face Screaming In Fear",
+        meaning: "A face with wide eyes and an open mouth, screaming in fear. It is often used to express shock, horror, or extreme surprise."
+      },
+
+      {
+        id: 8,
+        emoji: "😜",
+        name: "Winking Face With Tongue",
+        meaning: "A face with one eye closed and a tongue sticking out. It is often used to convey a sense of playfulness or joking."
+      },
+
+      {
+        id: 9,
+        emoji: "😇",
+        name: "Smiling Face With Halo",
+        meaning: "A face with a smile and a halo above its head. It is often used to convey innocence or goodness."
+      },
+
+      {
+        id: 10,
+        emoji: "😈",
+        name: "Smiling Face With Horns",
+        meaning: "A face with a smile and horns. It is often used to convey mischief or evil intent."
+      },
+
+      {
+        id: 11,
+        emoji: "😋",
+        name: "Face Savoring Food",
+        meaning: "A face with a smile and closed eyes, savoring food. It is often used to convey enjoyment of food or a delicious meal."
+      },
+
+      {
+        id: 12,
+        emoji: "😤",
+        name: "Face With Steam From Nose",
+        meaning: "A face with steam coming from its nose. It is often used to convey anger or frustration."
       }
 ]
 
